788b8c478321853b88716db7017056bb1a5e042f
JaviViCa <vicaxa2010@gmail.com>
2024-01-15 19:37:09 +0100

    fixed error

--- exercise5/exercise.js
+++ exercise5/exercise.js
@@ -4,9 +4,11 @@ const person1 = {
   age: 25,
 };
 
-const person2 = {};
-Object.assign(person2, person1);
-
+const person2 = person1;
+/* Cuando asignas person1 a la variable person2, realmente estás copiando la referencia
+en memoria de ese objeto. Por lo tanto, cuando modificas uno también modificas el otro,
+debido a que realmente haces referencia al mismo objeto. Una manera de solucionar esto es:
+const person2 = Object.assign({}, person1); */
 // Modify the property `firstName` of the `person2` in "Simon".
 person2.firstName = "Simon";
 
