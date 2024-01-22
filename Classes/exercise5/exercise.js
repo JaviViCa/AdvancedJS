ba50c98a319c6978b38b42baf34e53fe5b8e295c
JaviViCa <vicaxa2010@gmail.com>
2024-01-22 21:42:29 +0100

    completed exercise

--- Classes/exercise5/exercise.js
+++ Classes/exercise5/exercise.js
@@ -18,7 +18,17 @@ class Circle {
 }
 
 class AreaCalculator {
-  // ...
+  static calculate (figure) {
+    if (figure instanceof Square) {
+      return figure.side * figure.side; /* Math.pow(figure.side, 2) */
+    } else if (figure instanceof Rectangle){
+      return figure.height * figure.width;
+    } else if (figure instanceof Circle) {
+      return Math.PI * figure.radius * figure.radius; /* Math.PI * figure.radius ** 2 */
+    }else {
+      throw new Error("Unsupported figure type");
+    }
+  }
 }
 
 const square = new Square(4);
