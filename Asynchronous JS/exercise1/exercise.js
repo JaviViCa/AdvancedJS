a4cd1810fc22622fbcf4b8e9e001e9c21686de88
JaviViCa <vicaxa2010@gmail.com>
2024-02-05 20:32:22 +0100

    completed exercise

--- Asynchronous JS/exercise1/exercise.js
+++ Asynchronous JS/exercise1/exercise.js
@@ -1,1 +1,11 @@
+function printAsyncName(callback, name) {
+    setTimeout(() => {
+        callback();
+        setTimeout(() => {
+            console.log(name)
+        }, 2000)
+    }, 1000);
+}
 
+let callback = () => console.log("Hello");
+printAsyncName(callback, "Javi")
