ef3d4f820fb482252c2c582f71f2d18c09a84d78
JaviViCa <vicaxa2010@gmail.com>
2024-02-05 20:45:46 +0100

    completed exercise

--- Asynchronous JS/exercise2/exercise.js
+++ Asynchronous JS/exercise2/exercise.js
@@ -1,1 +1,10 @@
+function repeatHello(callback) {
+    setInterval(() => {
+        callback();
+        }, 1000);
+}
 
+let callback = () => console.log("Hello");
+// Al usar una función de flecha para el callback, nos aseguramos de que se evitan problemas de perdidas de contexto y que el código sea conciso y fácil de entender.
+
+repeatHello(callback)
