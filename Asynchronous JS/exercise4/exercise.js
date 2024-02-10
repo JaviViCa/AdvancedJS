61e20c05615cb1149a92e64d62e2460a8c60b059
JaviViCa <vicaxa2010@gmail.com>
2024-02-10 22:09:57 +0100

    completed exercise

--- Asynchronous JS/exercise4/exercise.js
+++ Asynchronous JS/exercise4/exercise.js
@@ -1,1 +1,12 @@
-const number = 15;
+let promise = new Promise((resolve, reject) => {
+    const number = -5;
+    if (number >= 10) {
+        resolve(number)
+    } else {
+        reject(number)
+    }
+})
+
+promise
+    .then((value) => console.log(`${value} is greater or equals than 10`))
+    .catch((error) => (console.error(`Error: ${error} is lower than 10`)))
--- Asynchronous JS/exercise6/exercise.js
+++ Asynchronous JS/exercise6/exercise.js
@@ -1,1 +1,26 @@
+const isLogged = true;
 
+function logging (isLogged) {
+    return new Promise((resolve, reject) => {
+        if (isLogged) {
+            resolve(Math.random());
+        } else {
+            reject(new Error("Error: Not logged user"));
+        }
+    });
+}
+function userLogged (num) {
+    return new Promise((resolve, reject) => {
+      if (num > 0.5) {
+        resolve({ name: "John", age: 24 });
+      } else {
+        reject(new Error(`Error: ${num} is lower or equals to 0.5`));
+      }
+    });
+  }
+
+logging(isLogged)
+  .then((randomNum) => userLogged(randomNum))
+  .then((user) => console.log(user))
+  .catch((error) => (console.error(error)))
+  .finally(() => console.log("End of the exercise!"))
